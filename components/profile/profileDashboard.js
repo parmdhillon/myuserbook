e96455df37d70600249550febeb8dccd5ad727dd
Parminder <iparm1996@gmail.com>
2021-09-01 22:12:59 -0400

    Added Profile Dashboard and Profile Page

---
+++ components/profile/profileDashboard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProfileDashboard = () => {
+  return (
+    <div>
+      <h1>Profile Dashboard</h1>
+    </div>
+  );
+};
+
+export default ProfileDashboard;
--- pages/profile.js
+++ pages/profile.js
@@ -1,18 +1,35 @@
 import React, { useEffect } from 'react';
-
+import { FaBook } from '@react-icons/all-files/fa/FaBook';
+import { HiOutlineLogout } from '@react-icons/all-files/hi/HiOutlineLogout';
 import { verifyToken, getCookieToken, signoutUser } from '../lib/utils';
 
 const profile = ({ profile }) => {
   return (
-    <div>
-      <h1>Profile</h1>
-      <button
-        onClick={() => {
-          signoutUser();
-        }}
-      >
-        Sign out
-      </button>
+    <div className="bg-white h-screen">
+      <div className="w-full flex flex-wrap">
+        <div className="w-full flex flex-col">
+          <div className="w-full flex justify-between">
+            <div className="flex justify-start pt-12 md:pl-12 md:-mb-12">
+              <span
+                className="bg-black text-white font-bold text-xl p-4"
+                alt="Logo"
+              >
+                <FaBook className="inline" /> My User Book
+              </span>
+            </div>
+            <div className="flex justify-end pt-12 md:pr-12 md:-mb-12">
+              <button
+                onClick={() => {
+                  signoutUser();
+                }}
+                className="bg-red-400 text-white font-bold text-xl p-4 hover:bg-red-500"
+              >
+                <HiOutlineLogout className="inline" /> Logout
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
